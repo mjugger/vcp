918ccba58586cc9258935ed168dcf228f672499b
mjugger <xaiox20@gmail.com>
2014-07-15 05:58:28 -0400

    corrected naming convention for post type

--- vcp.php
+++ vcp.php
@@ -1,30 +1,30 @@
 <?php
 	 /*
    Plugin Name: video custom post
-   Plugin URI: http://my-awesomeness-emporium.com
-   Description: a plugin to create awesomeness and spread joy
-   Version: 1.2
-   Author: Mr. Awesome
-   Author URI: http://mrtotallyawesome.com
-   License: GPL2
+   Plugin URI: https://github.com/mjugger/vcp
+   Description: Creates a custom post type for videos
+   Version: 1.0
+   Author: Mukhtar Jugger
+   Author URI: https://github.com/mjugger
+   License: ???
    */
   
    //Employee Bio post type.
-	add_action('init', 'employee_bio',2);
+	add_action('init', 'video_post',2);
 
-	     function employee_bio() {
+	     function video_post() {
 	       $feature_args2 = array(
 	          'labels' => array(
-	           'name' => __( 'Employee Bios' ),
-	           'singular_name' => __( 'Employee Bio' ),
-	           'add_new' => __( 'Add New Bio' ),
-	           'add_new_item' => __( 'Add New Bio' ),
-	           'edit_item' => __( 'Edit Bio' ),
-	           'new_item' => __( 'New Bio' ),
-	           'view_item' => __( 'View Bio' ),
-	           'search_items' => __( 'Search Employee Bios' ),
-	           'not_found' => __( 'No Employee Bio found' ),
-	           'not_found_in_trash' => __( 'No Employee Bio found in trash' )
+	           'name' => __( 'video posts' ),
+	           'singular_name' => __( 'video post' ),
+	           'add_new' => __( 'Add New video post' ),
+	           'add_new_item' => __( 'Add New video post' ),
+	           'edit_item' => __( 'Edit video post' ),
+	           'new_item' => __( 'New video post' ),
+	           'view_item' => __( 'View video post' ),
+	           'search_items' => __( 'Search video posts' ),
+	           'not_found' => __( 'No video posts found' ),
+	           'not_found_in_trash' => __( 'No video posts found in trash' )
 	         ),
 	       'public' => true,
 	       'show_ui' => true,
@@ -34,7 +34,7 @@
 	       'menu_position' => 20,
 	       'supports' => array('title', 'editor', 'thumbnail','excerpt','page-attributes')
 	     );
-	  register_post_type('bio',$feature_args2);
+	  register_post_type('video post',$feature_args2);
 	}
 
   
